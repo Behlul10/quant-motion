e929273dcfd56b7ec53669ac2d10ad9060f20922
Behlul10 <behlulvardal@gmail.com>
2026-03-27 15:38:18 -0400

    Update Landmarks.py (finished the landmark processor)
    In this update you can now use the landmarks.py script to process any video file and load all the landmarks into a json file. The landmarks also have been timestamped.

--- src/landmarks.py
+++ src/landmarks.py
@@ -1,11 +1,9 @@
 model_path = "D:/new_quant/models/hand_landmarker.task"
 video_path = "D:/new_quant/data/videos/hand_wave.mp4"
-output_path = "data/raw/hand_wave.json"
+output_path = "D:/new_quant/data/raw/hand_wave.json"
 
 
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 import cv2
 import json
 
@@ -20,6 +18,8 @@ def extract_hand_landmarks(video_path, model_path, output_path):
     HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
     VisionRunningMode = mp.tasks.vision.RunningMode
 
+    landmarks = []
+
     # Create a hand landmarker instance with the video mode:
     options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path= model_path),
@@ -31,8 +31,19 @@ def extract_hand_landmarks(video_path, model_path, output_path):
         #     print(f"frame shape: {frame.shape}")
         while ret == True:
             ret, frame = cap.read()
+            if ret == False:
+                break
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+            timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+            result = landmarker.detect_for_video(image, timestamp_ms)
+            if result.hand_landmarks: #if list is not empty
+                temp_list = []
+                for point in result.hand_landmarks[0]:
+                    temp_list.append([point.x, point.y, point.z])
+                landmarks.append({"timestamp": timestamp_ms, "landmarks": temp_list})
+    with open(output_path, "w") as f:
+        json.dump(landmarks, f, indent=2)
 
 if __name__ == "__main__":
     extract_hand_landmarks(video_path, model_path, output_path)
